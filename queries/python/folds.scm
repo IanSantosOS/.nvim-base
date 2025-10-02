6de5372a51b872522ad0c40e409dddc2e7d85ebd
IanSantosOS <iann.silva2006@gmail.com>
2025-10-02 00:49:45 -0300

    queries: update python folds query

--- queries/python/folds.scm
+++ queries/python/folds.scm
@@ -1,10 +1,10 @@
 ;; This file defines folds for Python docstrings only.
 
 ; Docstrings in modules
-(module body: (block. (expression_statement (string) @fold)))
+(module (expression_statement (string) @fold))
 
 ; Docstrings in classes
-(class_definition body: (block. (expression_statement (string) @fold)))
+(class_definition body: (block . (expression_statement (string) @fold)))
 
 ; Docstrings in functions and methods
-(function_definition body: (block. (expression_statement (string) @fold)))
+(function_definition body: (block . (expression_statement (string) @fold)))
